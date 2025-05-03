9e677c8665dcaf169ff257ff9a9eb9f64202d197
Degs1 <106155913+Degs1@users.noreply.github.com>
2025-05-04 07:47:06 +0800

    Optimize brat generator

--- bot wa wwebjs/lib/brat.js
+++ bot wa wwebjs/lib/brat.js
@@ -6,8 +6,9 @@ export const brat = async (msg, text) => {
   try {
     if (!text) throw new Error("missing text input");
 
-        const res = await axios.get(`https://api.siputzx.my.id/api/m/brat?text=${text}&isVideo=false&delay=0`, {
-            responseType: "arraybuffer"
+       // const res = await axios.get(`https://api.siputzx.my.id/api/m/brat?text=${text}&isVideo=false&delay=0`, {
+          const res = await axios.get(`https://brat.caliphdev.com/api/brat?text=${text}`, {
+	  responseType: "arraybuffer"
         });
 		//console.log(res);
         const image = Buffer.from(res.data); // Alternatif pengganti Buffer di ESM
